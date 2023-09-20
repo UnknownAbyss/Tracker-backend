3211778304f0813661d6ca7aa491565a13052b50
Additya Singhal <addityadd@gmail.com>
2023-09-20 16:09:34 +0530

    feat: favicon

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use('/favicon.ico', express.static('public/favicon.ico'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
